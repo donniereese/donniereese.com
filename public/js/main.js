a9d48694011eb9bdae6c9ea65586cac6cfad474a
Donnie Reese <donniereese@gmail.com>
2018-09-01 17:51:23 -0500

    Added form checking and submission.  statuses are checked. Just have to tweak styles for the messages.

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,95 @@
 // Imperative functionality, programming in more reliable es5
 // very vanilla javascript is less-likely to break
-console.log('bananaroaming');
-
 (function() {
+    var contactForm = document.getElementById('contact-form');
+    var inputs = {
+        name: contactForm.querySelector('input[name=contactName]'),
+        email: contactForm.querySelector('input[name=contactEmail]'),
+        message: contactForm.querySelector('textarea[name=contactMessage]'),
+        submit: contactForm.querySelector('button[name=contactSubmit]')
+    }
+
+    var formStatus = {
+        name: {
+            noEmpty: { type: 'error', status: false, el: null, message: 'Your name, if you might.' }
+        },
+        email: {
+            noEmpty: { type: 'error', status: false, el: null, message: "If you give me your email I can say hi." },
+            wrongFormat: { type: 'error', status: false, el: null }
+        },
+        message: {
+            noEmpty: { type: 'error', status: false, el: null, message: 'Did you not have anything to say?' }
+        },
+        submit: {
+            waiting: { type: 'wait', status: false, el: null, message: 'Pneumatic tube is wooshin your message away.' },
+            success: { type: 'success', status: false, el: null, message: "Huzzah! I'll see your message shortly." },
+            failed: { type: 'error', status: false, el: null, message: "Oh no! Looks like it failed. Want to try again?" }
+        }
+    }
+
+    function createElement(type = 'message', contents = '', hasIcon = true) {
+
+        var el = document.createElement('span');
+            el.classList.add('message');
+        var icon = null;
+        var fontIcon = null;
+
+        if (hasIcon) {
+            icon = document.createElement('strong');
+            icon.classList.add('message-accent');
+            fontIcon = document.createElement('i');
+            fontIcon.classList.add('fas');
+            icon.appendChild(fontIcon);
+            el.appendChild(icon);
+        }
+
+        switch (type) {
+            case 'wait':
+                el.classList.add('wait-message');
+                if (hasIcon) { fontIcon.classList.add('fa-spinner', 'fa-pulse'); icon.classList.add('wait-accent'); }
+                break;
+            case 'error':
+                el.classList.add('error-message');
+                if (hasIcon) { fontIcon.classList.add('fa-times'); icon.classList.add('error-accent'); }
+                break;
+            case 'alert':
+                el.classList.add('alert-message');
+                if (hasIcon) { fontIcon.classList.add('fa-exclamation-triangle'); icon.classList.add('alert-accent'); }
+                break;
+            case 'success':
+                el.classList.add('success-message');
+                if (hasIcon) { fontIcon.classList.add('fa-check'); icon.classList.add('success-accent'); }
+                break;
+            case 'message':
+            default:
+                el.classList.add('message-common');
+                if (hasIcon) fontIcon.classList.add('fa-comment');
+        }
+
+        var textNode = document.createTextNode(contents);
+        el.appendChild(textNode);
+
+        return el;
+    }
+
+    function changeStatusOf(target, messageId, status) {
+        var stat = formStatus[target][messageId];
+
+        if (!status) {
+            console.log('brag', target, status, stat)
+            if (stat.el && stat.el instanceof HTMLElement) stat.el.parentNode.removeChild(stat.el);
+            stat.status = false;
+
+        } else {
+            console.log('banter', target, status, stat);
+            if (stat.status !== status){
+                stat.el = createElement(stat.type, stat.message, true);
+                stat.status = true;
+                inputs[target].parentNode.appendChild(stat.el);
+            }
+        }
+    }
+
     function sanitarize(string) {
         const map = {
             '&': '&amp;',
@@ -16,24 +103,46 @@ console.log('bananaroaming');
         return string.replace(reg, (match)=>(map[match]));
     }
 
-    var contactForm = document.getElementById('contact-form');
+    inputs.name.addEventListener('blur', function(e) {
+        // if (e.target.value === '') changeStatusOf('name', 'noEmpty', true);
+    });
+
+    inputs.email.addEventListener('blur', function(e) {
+        console.log('Email Blur');
+        if (e.target.value === '') {
+            changeStatusOf('email', 'noEmpty', true);
+        } else {
+            changeStatusOf('email', 'noEmpty', false);
+        }
+    });
+
+    inputs.message.addEventListener('blur', function(e) {
+        console.log('Message Blur')
+        if (e.target.value === '') {
+            changeStatusOf('message', 'noEmpty', true)
+        } else {
+            changeStatusOf('message', 'noEmpty', false)
+        }
+    });
 
     contactForm.addEventListener('submit', function(e) {
         e.preventDefault();
 
-        var name = contactForm.querySelector('input[name=contactName]').value;
-        var email = contactForm.querySelector('input[name=contactEmail]').value;
-        var message = contactForm.querySelector('textarea[name=contactMessage]').value;
+        changeStatusOf('submit', 'waiting', true);
 
-        name = sanitarize(name);
-        email = sanitarize(email);
-        message = sanitarize(message);
+        var name = sanitarize(inputs.name.value);
+        var email = sanitarize(inputs.email.value);
+        var message = sanitarize(inputs.message.value);
 
         axios.post('http://localhost:3000/api/hello', { name, email, message })
         .then(function (response) {
+            changeStatusOf('submit', 'waiting', false);
+            changeStatusOf('submit', 'success', true);
             console.log(response);
         })
         .catch(function (error) {
+            changeStatusOf('submit', 'waiting', false);
+            changeStatusOf('submit', 'failed', true);
             console.log(error);
         });
     })
